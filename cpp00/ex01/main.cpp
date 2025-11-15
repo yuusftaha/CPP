2dd9f07a42165b665b909fc17968a77e6624d70c
Yusuf Taha Özlü <yozlu@k1m07s04.42istanbul.com.tr>
2025-11-15 19:58:44 +0300

    dosyalar düzenlendi

---
+++ cpp00/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "PhoneBook.hpp"
+
+int main()
+{
+	while (1)
+	{
+		std::cout << "*******************************************************" << std::endl;
+		std::cout << "*               WELCOME TO MY PHONEBOOK               *" << std::endl;
+		std::cout << "* Write what you want to do following three operation *" << std::endl;
+		std::cout << "*           ADD    |    SEARCH    |    EXIT           *" << std::endl;
+		std::cout << "*******************************************************" << std::endl;
+
+		std::
+	}
+	
+
+	return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	std::string firstname;
+	std::string lastname;
+	int a;
+
+	std::cout << "adınız ve soyadınızı giriniz" << std::endl;
+
+	std::cin >> a;
+
+	std::cout << a << " " << lastname << std::endl;
+	return 0;
+}
