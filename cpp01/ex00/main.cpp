453a85d9345d7fa22f430f11649bbca16ab5bd85
yusuf <yutahaozlu@gmail.com>
2025-12-20 23:30:48 +0300

    cpp01 e baslandi

---
+++ cpp01/ex00/main.cpp
@@ -0,0 +1,12 @@
+#include "Zombie.hpp"
+
+int main()
+{
+    randomChump("Foo");
+    std::cout << "--------------------------------" << std::endl;
+    Zombie* heapZombie = newZombie("Bar");
+    heapZombie->announce();
+    delete heapZombie;
+
+    return 0;
+}
