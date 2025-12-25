5a938d5cc81a7dff2a5501a115dca1537edfa783
yusuf <yutahaozlu@gmail.com>
2025-12-25 17:00:53 +0300

    ex01 ex02 yapildi

---
+++ cpp01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::string str = "HI THIS IS BRAIN";
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
+    std::cout << "Address of the string: " << &str << std::endl;
+    std::cout << "Address held by stringPTR: " << stringPTR << std::endl;
+    std::cout << "Address held by stringREF: " << &stringREF << std::endl;
+    std::cout << "Value of the string: " << str << std::endl;
+    std::cout << "Value pointed to by stringPTR: " << *stringPTR << std::endl;
+    std::cout << "Value pointed to by stringREF: " << stringREF << std::endl;
+    return 0;
+}
